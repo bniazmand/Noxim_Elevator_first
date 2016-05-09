dfc5d822b0d9f14911b18ce81bb6ecbc8881982e
Behrad Niazmand <bniazmand@Behrads-MacBook-Pro.local>
2016-05-09 16:55:04 +0300

    Scripts Update

--- scripts/Data_collection_scripts/throughput_script.sh
+++ scripts/Data_collection_scripts/throughput_script.sh
@@ -11,7 +11,7 @@ do
 if [[ $counter -lt 1 ]]
 then
 awk '{ print $1 }' $f > temp1.txt
-sed -n 8,77p temp1.txt > $ResultFile
+sed -n 8,17p temp1.txt > $ResultFile
 rm temp1.txt
 fi
 
@@ -23,7 +23,7 @@ inputNo=$counter
 fi
 
 awk '{ print $3 }' $f > temp.txt
-sed -n 8,77p temp.txt > throughput_result_$inputNo.dot
+sed -n 8,17p temp.txt > throughput_result_$inputNo.dot
 rm temp.txt
 counter=`expr $counter + 1`
 done
@@ -37,7 +37,7 @@ cp throughput_result_temp.txt $ResultFile
 rm throughput_result_temp.txt
 done
 
-echo "pir	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25	26	27	28	29	30	31	32	33	34	35	36	37	38	39	40	41	42	43	44	45	46	47	48	49" > new_file.txt
+echo "pir	Elevator_First" > new_file.txt
 cat $ResultFile >> new_file.txt
 cp new_file.txt $ResultFile
 
--- scripts/noxim_script.sh
+++ scripts/noxim_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash    
-counter=0
        mkdir Results
        cd Results/
        mkdir bitreversal
@@ -9,34 +8,19 @@ counter=0
        mkdir transpose1
        mkdir transpose2
        cd ..
-    while read variable  
-    do  
-       echo $variable > all_2d_turn_model_LBDR_bits.txt
+
        ./noxim_explorer sim.cfg
 
-       if [ $counter -ge 10 ]; then
-        cp routing_lbdr__sel_random__topology_4x4__traffic_bitreversal__.m  ./Results/bitreversal/r_bitreversal_$counter.m
-        cp routing_lbdr__sel_random__topology_4x4__traffic_butterfly__.m  ./Results/butterfly/r_butterfly_$counter.m
-        cp routing_lbdr__sel_random__topology_4x4__traffic_random__.m  ./Results/random/r_random_$counter.m
-        cp routing_lbdr__sel_random__topology_4x4__traffic_shuffle__.m  ./Results/shuffle/r_shuffle_$counter.m
-        cp routing_lbdr__sel_random__topology_4x4__traffic_transpose1__.m  ./Results/transpose1/r_transpose1_$counter.m
-        cp routing_lbdr__sel_random__topology_4x4__traffic_transpose2__.m  ./Results/transpose2/r_transpose2_$counter.m
-       else
-        cp routing_lbdr__sel_random__topology_4x4__traffic_bitreversal__.m  ./Results/bitreversal/r_bitreversal_0$counter.m
-        cp routing_lbdr__sel_random__topology_4x4__traffic_butterfly__.m  ./Results/butterfly/r_butterfly_0$counter.m
-        cp routing_lbdr__sel_random__topology_4x4__traffic_random__.m  ./Results/random/r_random_0$counter.m
-        cp routing_lbdr__sel_random__topology_4x4__traffic_shuffle__.m  ./Results/shuffle/r_shuffle_0$counter.m
-        cp routing_lbdr__sel_random__topology_4x4__traffic_transpose1__.m  ./Results/transpose1/r_transpose1_0$counter.m
-        cp routing_lbdr__sel_random__topology_4x4__traffic_transpose2__.m  ./Results/transpose2/r_transpose2_0$counter.m
-       fi
-       echo $counter
-       counter=`expr $counter + 1`
+        cp elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_bitreversal__.m  ./Results/bitreversal/r_bitreversal__.m
+        cp elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_butterfly__.m  ./Results/butterfly/r_butterfly__.m
+        cp elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_random__.m  ./Results/random/r_random__.m
+        cp elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_shuffle__.m  ./Results/shuffle/r_shuffle__.m
+        cp elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_transpose1__.m  ./Results/transpose1/r_transpose1__.m
+        cp elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_transpose2__.m  ./Results/transpose2/r_transpose2__.m
 
-    done <	all_2d_turn_models.txt
-
-       rm routing_lbdr__sel_random__topology_4x4__traffic_bitreversal__.m
-       rm routing_lbdr__sel_random__topology_4x4__traffic_butterfly__.m
-       rm routing_lbdr__sel_random__topology_4x4__traffic_random__.m
-       rm routing_lbdr__sel_random__topology_4x4__traffic_shuffle__.m
-       rm routing_lbdr__sel_random__topology_4x4__traffic_transpose1__.m
-       rm routing_lbdr__sel_random__topology_4x4__traffic_transpose2__.m
+       rm elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_bitreversal__.m
+       rm elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_butterfly__.m
+       rm elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_random__.m
+       rm elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_shuffle__.m
+       rm elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_transpose1__.m
+       rm elevator_nodes_Elevator_nodes_scenarios_elevator_nodes_20_percent_txt__routing_xyz__sel_random__topology_4x4x4__traffic_transpose2__.m
